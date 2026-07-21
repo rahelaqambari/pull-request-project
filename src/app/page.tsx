032ac49da69f096811c4e5f39efc2c0c3ea4c454
rahelaqambari <rahelaqambari@gmail.com>
2026-07-21 21:59:50 +0430

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='w-full h-screen flex items-center justify-center '>
-      <h1 className='text-2xl bg-linear-150 text-transparent animate-pulse from-green-700 to-orange-700 via-red-200'>Home Page</h1>
+    <div className='w-full relative bg-black h-screen flex items-center justify-center '>
+      <h1 className='text-5xl bg-linear-150 p-8  text-transparent bg-clip-text  animate-pulse from-green-700 to-orange-700 via-red-200'>Home Page</h1>
+      {/* <div className=' h-40 w-40 right-6 top-8 rounded-full bg-linear-150  animate-pulse from-green-700 to-orange-700 via-red-200 border-2'></div> */}
     </div>
   )
 }
