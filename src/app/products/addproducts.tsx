609c408c043a98d41c9906bc935b6b1e37c1a148
rahelaqambari <rahelaqambari@gmail.com>
2026-07-30 22:25:01 +0430

    new pages

---
+++ src/app/products/addproducts.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function AddProduct() {
+  return (
+    <div className='w-full'>
+      <form action="" className='w-8/12 flex flex-col items-center p-6'>
+        <div>
+            <label htmlFor="Name">Name</label>
+            <input type="text"  />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default AddProduct
