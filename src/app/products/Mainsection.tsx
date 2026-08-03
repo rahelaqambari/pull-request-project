5bf07505ba51b9b3a53e45795ef71aa0f56c721e
rahelaqambari <rahelaqambari@gmail.com>
2026-08-03 21:45:42 +0430

    ne changes

---
+++ src/app/products/Mainsection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddProduct from './addproducts'
+
+function Mainsection() {
+  return (
+    <div className='h-full w-full flex items-center'>
+      <AddProduct />
+    </div>
+  )
+}
+
+export default Mainsection
