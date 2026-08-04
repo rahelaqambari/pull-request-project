d3796b961da241adb1c07a32a6bb6ebfa44a5f33
rahelaqambari <rahelaqambari@gmail.com>
2026-08-04 21:27:15 +0430

    geting all products

---
+++ src/app/products/Allproduct.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function AllProducts() {
+  return (
+    <div>
+      <table>
+        <tr>
+            <th>Name</th>
+            <th>price</th>
+            <th>category</th>
+            <th>brand</th>
+            <th>stock</th>
+        </tr>
+        <tr>
+            <td></td>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+export default AllProducts
